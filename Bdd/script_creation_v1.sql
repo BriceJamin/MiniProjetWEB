3dc4b1844004eb8507010391619aa455aa31ebcb
SmallFitz <smallfitz@hotmail.fr>
2012-03-18 16:39:06 +0100

    Demarrage du projet

---
+++ Bdd/script_creation_v1.sql
@@ -0,0 +1,46 @@
+Classe (BTS SE OU IRIS) 
+?  Nom, prénom, date de naissance 
+?  N° de téléphone, portable 
+?  Adresse mail 
+?  Date d'entrée/sortie de l'établissement 
+?  Entreprise de stage 
+?  Parcours POST BTS 
+?  Un champ information libre. 
+
+GRANT ALL PRIVILEGES ON `bdd_ent` . * TO 'user_ent' IDENTIFIED BY 'mdp_ent' WITH GRANT OPTION;
+
+DROP database IF EXISTS bdd_ent;
+
+CREATE database IF NOT EXISTS bdd_ent CHARACTER SET utf8 COLLATE utf8_bin;
+
+USE bdd_ent;
+
+-- --------------------------------------------------------
+--
+-- 		ELEVE
+--
+
+CREATE TABLE if not exists etudiant
+(
+	id				INT				UNSIGNED 	NOT NULL 		AUTO_INCREMENT,
+	nom 			VARCHAR(40) 				NOT NULL,
+	prenom			VARCHAR(40) 				NOT NULL,
+	dateNaissance	DATE,
+	telFixe			VARCHAR(40),
+	telPortable		VARCHAR(40),
+	email			VARCHAR(40),
+	dateArrivee		DATE,
+	dateDepart		DATE,
+	
+	PRIMARY KEY(id)
+	
+)CHARACTER SET utf8 TYPE=InnoDB;
+
+--
+-- Contenu de la table eleve
+--
+
+INSERT IGNORE INTO eleve
+	(nom)
+VALUES
+	("Hdv des alchimistes"),
---
+++ Site/index.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Accueil</title>
+	</head>
+	<body>
+		<h1>Accueil</h1>
+		<form method="POST" action="connexion.php">
+			Nom :<input type="text" name="nom" />
+			Mot de passe :<input type="password" name="mdp" />
+			<input type="button" value="Se connecter" />
+		</form>
+	</body>
+</html>
