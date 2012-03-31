81e992a06eb3739024b706377c21876b01763371
SmallFitz <smallfitz@hotmail.fr>
2012-03-31 11:21:24 +0200

    Creation actions.php

---
+++ Site/actions.php
@@ -0,0 +1,6 @@
+<?php
+	/*
+		Page d'accueil une fois qu'on est connecté.
+		Liste les actions qu'il est possible de faire.
+		Exemple : Afficher le profil, chercher un profil particulier... Etc.
+	*/
