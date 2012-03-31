4b72473932eba40fc0be71ef44943849e0857a87
SmallFitz <smallfitz@hotmail.fr>
2012-03-31 16:17:56 +0200

    index.php redirection remplacant l'inclusion

--- Site/index.php
+++ Site/index.php
@@ -3,6 +3,5 @@
 		C'est le fichier qui est interprété par défaut quand on arrive sur le site.
 		Permet de choisir sur quelle page l'utilisateur arrive en premier.
 	*/
-	include 'accueil.php';
-	echo $page;
+	header('connexion.php');
 ?>
