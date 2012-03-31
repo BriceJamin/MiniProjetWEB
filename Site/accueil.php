207f17c114d167f47ab8cebe22a8e12e15afe621
SmallFitz <smallfitz@hotmail.fr>
2012-03-31 11:35:00 +0200

    Modif explication du role de accueil

--- Site/accueil.php
+++ Site/accueil.php
@@ -1,7 +1,7 @@
 <?php
 /* 
-	Cette page permet de se connecter.
-	Si le client n'est pas connecté il est automatiquement redirigé ici. 
+	Cette page permet à l'utilisateur de rentrer ses identifiants.
+	Si l'utilisateur n'est pas connecté il est automatiquement redirigé ici.
 */
 
 $page = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
