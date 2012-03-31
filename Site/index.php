3b79de9ad2b5eb778f1cbc0799112983d4b88655
SmallFitz <smallfitz@hotmail.fr>
2012-03-31 11:41:33 +0200

    Ajout explication role index

--- Site/index.php
+++ Site/index.php
@@ -1,4 +1,8 @@
 ﻿<?php
+	/*
+		C'est le fichier qui est interprété par défaut quand on arrive sur le site.
+		Permet de choisir sur quelle page l'utilisateur arrive en premier.
+	*/
 	include 'accueil.php';
 	echo $page;
 ?>
