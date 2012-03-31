8f73f3ee09963a776152ae6b42aaf92b4a9d309b
SmallFitz <smallfitz@hotmail.fr>
2012-03-31 11:19:45 +0200

    Creation messageInfo

---
+++ Site/messageInfo.php
@@ -0,0 +1,6 @@
+<?php
+	/* 
+		Sert à detecter les messages à afficher à l'utilisateur
+		Exemple, message de bienvenue, d'arreur, d'aurevoir... etc
+		Il sera à inclure dans chaque page censée recevoir un message
+	*/
