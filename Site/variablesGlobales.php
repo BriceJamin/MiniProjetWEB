80e165cb227cd37bab65edec19639b33f94ec853
SmallFitz <smallfitz@hotmail.fr>
2012-04-11 13:22:13 +0200

    Utilisation du mot clef GLOBAL

--- Site/variablesGlobales.php
+++ Site/variablesGlobales.php
@@ -1,28 +1,31 @@
 <?php
-	$PAGE_PAR_DEFAUT = "accueil.php";
+	if(!isset($GLOBALS['PAGE_PAR_DEFAUT']))
+		$GLOBALS['PAGE_PAR_DEFAUT'] = "accueil.php";
 	
-	if(!isset($DOCTYPE))
-		$DOCTYPE =
+	if(!isset($GLOBALS['DOCTYPE']))
+		$GLOBALS['DOCTYPE'] =
 			'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 			<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">';
 
-	if(!isset($TITLE))
-		$TITLE = '';
+	if(!isset($GLOBALS['TITLE']))
+		$GLOBALS['TITLE'] = '';
 
-	if(!isset($HEAD))
-		$HEAD =
+	if(!isset($GLOBALS['HEAD']))
+		$GLOBALS['HEAD'] =
 				'<head>
-					<title>'.$TITLE.'</title>
-					<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+					<title>'.$GLOBALS['TITLE'].'</title>
+					<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 				</head>';
 
-	if(!isset($BODY))
-		$BODY = "<body>";
+	if(!isset($GLOBALS['BODY']))
+		$GLOBALS['BODY'] = "<body>";
 
-	if(!isset($PIED))
-		$PIED = '</body></html>';
+	if(!isset($GLOBALS['PIED']))
+		$GLOBALS['PIED'] = '</body></html>';
 	
-	if(!isset($NBR_CHAR_MAX_NOM))			$NBR_CHAR_MAX_NOM=20;
-	if(!isset($NBR_CHAR_MAX_MDP))			$NBR_CHAR_MAX_MDP=40;
-	if(!isset($LONGUEUR_CHAMPS)) 			$LONGUEUR_CHAMPS=15;
+	if(!isset($GLOBALS['NBR_CHAR_MAX_NOM']))	$GLOBALS['NBR_CHAR_MAX_NOM']=20;
+	if(!isset($GLOBALS['NBR_CHAR_MAX_MDP']))	$GLOBALS['NBR_CHAR_MAX_MDP']=40;
+	if(!isset($GLOBALS['LONGUEUR_CHAMPS'])) 	$GLOBALS['LONGUEUR_CHAMPS']=15;
+	
+	if(!isset($GLOBALS['BDD_CONNECTEE']))	$GLOBALS['BDD_CONNECTEE'] = false;
 ?>
